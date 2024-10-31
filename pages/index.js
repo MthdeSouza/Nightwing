1cd3184cc312e1d9ae6d026d4ef384abd392d526
Matheus Souza <a.matheussouza@gmail.com>
2024-10-31 12:07:53 -0300

    feat: profile image

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,39 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
-
+import { Container, Box, Heading, Image } from '@chakra-ui/react'
+import { useColorModeValue } from '@chakra-ui/react'
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+      <Box borderRadius="lg" bg={useColorModeValue("#FFFACD", "#556B2F")} p={3} mb={6} align="center" mt={2}>
         Hello, I'm an front-end developer based in Campinas!
       </Box>
 
-      <Box display={{ md: 'flex' }}> </Box>
-      <Box flexGrow={1}></Box>
-      <Heading as="h2" variant="page-title">
-        Matheus Souza
-      </Heading>
-      <p>Front-end Developer</p>
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Matheus Souza
+          </Heading>
+
+          <p>Front-end Developer</p>
+        </Box>
+        <Box textAlign="center" w={{ base: "100%", md: "unset" }}>
+          <Box
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            w="100px"
+            h="100px"
+            display="inline-block"
+            borderRadius="full"
+            overflow="hidden"
+            mt={{ base: 4, md: 0 }}
+          >
+            <Image
+              alt="Profile image"
+              src="/images/matheus.jpg"
+            />
+          </Box>
+        </Box>
+      </Box>
     </Container>
   )
 }
